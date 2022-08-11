f45414a0ca2566bf12c4bbdf61fd66f951c6baa3
Gabe Klein <gabe@termtm.com>
2022-08-11 00:17:43 -0400

    syntax

--- src/Query.ts
+++ src/Query.ts
@@ -22,10 +22,10 @@ namespace Query {
     T extends Field.Assertions<infer A> ? A : never;
 
   export type Where<T extends {}> =
-   & { [K in Exclude<keyof T, keyof Entity>]: WhereClause<T[K]> }
-   & {
-    has(values: Partial<T>): void;
-  }
+    & { [K in Exclude<keyof T, keyof Entity>]: WhereClause<T[K]> }
+    & {
+      has(values: Partial<T>): void;
+    }
 
   export type Normalize =
     (row: { [selection: string]: any }, output: any) => void;
