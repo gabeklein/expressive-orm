8c6f953ebb25ae8196eebfbb82224de8349a20f8
Gabe Klein <gabe@termtm.com>
2022-10-03 17:40:43 -0400

    implement Update query

---
+++ src/query/Update.test.ts
@@ -0,0 +1,28 @@
+import Entity, { VarChar } from "..";
+import Update from "./Update";
+
+class Foo extends Entity {
+  value = VarChar();
+  color = VarChar();
+}
+
+it("will generate query", () => {
+  const query = new Update(Foo, (where, foo) => {
+    where.equal(foo.color, "red");
+
+    return {
+      value: "Updated!",
+      color: "blue"
+    }
+  });
+
+  expect(query).toMatchInlineSnapshot(`
+UPDATE
+  \`Foo\`
+SET
+  \`value\` = "Updated!",
+  \`color\` = "blue"
+WHERE
+  \`Foo\`.\`color\` = 'red'
+`);
+})
---
+++ src/query/Update.ts
@@ -0,0 +1,71 @@
+import Entity from "..";
+import Field from "../Field";
+import { qualify } from "../utility";
+import Query, { serialize } from "./Query";
+
+declare namespace Update {
+  type Expect<T extends Entity> = { [K in Entity.Field<T>]?: T[K] };
+  type Values<T extends Entity> = Expect<T> | Expect<T>[];
+  type Function<T extends Entity> = (where: Query.Where, entity: Query.Fields<T>) => Values<T>;
+}
+
+class Update<T extends Entity> extends Query {
+  values: Map<Field, any>;
+
+  constructor(
+    public type: Entity.Type<T>,
+    from: Update.Function<T>){
+
+    super();
+
+    const entity = this.use(type);
+    const update = from(this.interface, entity);
+    const values = {} as any;
+
+    this.commit();
+
+    Object.entries(update).forEach((entry) => {
+      const [key, value] = entry;
+      const field = this.type.table.fields.get(key);
+
+      if(!field)
+        throw new Error(`Property ${key} has no corresponding field in entity ${type.name}`);
+
+      values[field.column] = field.set ? field.set(value) : value;
+    });
+
+    this.values = values;
+  }
+
+  toString(){
+    const tableName = qualify(this.type.table.name);
+    const values = [] as string[];
+
+    Object.entries(this.values).forEach(entry => {
+      const [column, value] = entry;
+
+      values.push(`${qualify(column)} = ${serialize(value)}`);
+    })
+
+    let sql = `UPDATE ${tableName} SET ${values.join(", ")}`;
+
+    if(this.tables.length > 1 || this.tables[0].alias)
+      sql += " " + this.generateTables();
+
+    sql += " " + this.generateWhere();
+
+    return sql;
+  }
+
+  exec(){
+    const { table, name } = this.type;
+    const conn = table.connection;
+
+    if(!conn)
+      throw new Error(`Query has no connection, have you setup entity ${name}?`);
+
+    return conn.query(this.toString());
+  }
+}
+
+export default Update;
