5d277a90d6a2c1eb3ccd1dc4d5dab3f3970f641d
Gabe Klein <gabe@termtm.com>
2022-10-12 23:34:45 -0400

    add select tests

--- tests/select.test.ts
+++ tests/select.test.ts
@@ -1,4 +1,4 @@
-import Entity, { Query, VarChar } from '../src';
+import Entity, { Int, One, Query, VarChar } from '../src';
 
 class Foo extends Entity {
   bar = VarChar();
@@ -55,6 +55,75 @@ SELECT
   \`Foo\`.\`bar\` AS \`1\`
 FROM
   \`Foo\`
+`);
+  })
+})
+
+describe("joins", () => {
+  class Foo extends Entity {
+    name = VarChar();
+    color = VarChar();
+  }
+
+  it("will select joined values", async () => {
+    class Bar extends Entity {
+      name = VarChar();
+      color = VarChar();
+      rating = Int();
+    }
+    
+    class Baz extends Entity {
+      rating = Int();
+    }
+  
+    const query = new Query(where => {
+      const foo = where(Foo);
+      const bar = where(Bar, { color: foo.color });
+      const baz = where(Baz, "left", { rating: bar.rating });
+  
+      where(foo.name).not("Danny");
+      where(bar.rating).greater(50);
+  
+      return where.get({
+        fooValue: foo.name,
+        barValue: bar.name,
+        bazRating: baz.rating
+      })
+    });
+  
+    expect(query).toMatchInlineSnapshot(`
+  SELECT
+    \`Foo\`.\`name\` AS \`fooValue\`,
+    \`Bar\`.\`name\` AS \`barValue\`,
+    \`Baz\`.\`rating\` AS \`bazRating\`
+  FROM
+    \`Foo\`
+    JOIN \`Bar\` ON \`Bar\`.\`color\` = \`Foo\`.\`color\`
+    LEFT JOIN \`Baz\` ON \`Baz\`.\`rating\` = \`Bar\`.\`rating\`
+  WHERE
+    \`Foo\`.\`name\` <> 'Danny'
+    AND \`Bar\`.\`rating\` > 50
+  `);
+  })
+
+  it("will select implicit joined values", async () => {
+    class Bar extends Entity {
+      name = VarChar();
+      foo = One(Foo);
+    }
+  
+    const query = new Query(where => {
+      const bar = where(Bar);
+  
+      return where.get(bar.foo.name)
+    });
+  
+    expect(query).toMatchInlineSnapshot(`
+SELECT
+  \`Foo\`.\`name\` AS \`1\`
+FROM
+  \`Bar\`
+  LEFT JOIN \`Foo\` ON \`Foo\`.\`id\` = \`Bar\`.\`fooId\`
 `);
   })
 })
