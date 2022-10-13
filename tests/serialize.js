bfe2824679589b2ad2b2e05658c37fd94096e95b
Gabe Klein <gabe@termtm.com>
2022-10-12 22:58:27 -0400

    simplify snapshot serialize

--- tests/serialize.js
+++ tests/serialize.js
@@ -1,25 +1,6 @@
 const { format } = require('sql-formatter');
 
-function test(){
-  return true;
-}
-
-function print(stuff){
-  if(typeof stuff !== "string")
-    return String(stuff);
-  
-  return stuff;
-}
-
-function serialize(stuff){
-  if(typeof stuff !== "string")
-    stuff = String(stuff);
-  
-  return format(stuff);
-}
-
 module.exports = {
-  test,
-  print,
-  serialize
+  test: () => true,
+  serialize: x => format(String(x))
 }
