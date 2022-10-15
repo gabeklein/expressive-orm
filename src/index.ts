64827be82320476774f51dbd06fc583626ad8f55
Gabe Klein <gabe@termtm.com>
2022-10-15 18:48:33 -0400

    expose Connections

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,9 @@ import Primary from './fields/Primary';
 import Ref from './fields/Ref';
 import Sub from './fields/Sub';
 import Table from './fields/Table';
+import MySQLConnection from './mysql/Connection';
 import Query from './query/Query';
+import SQLiteConnection from './sqlite/Connection';
 
 export * from './mysql';
 
@@ -17,10 +19,12 @@ export {
   Connection,
   Entity,
   Many,
+  MySQLConnection,
   One,
   Primary,
   Query,
   Ref,
+  SQLiteConnection,
   Sub,
   Table
 }
