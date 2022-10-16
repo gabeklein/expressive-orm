5d1bd0da9ee0625a56cb81ef032441f4722d9c91
Gabe Klein <gabe@termtm.com>
2022-10-16 16:27:03 -0400

    rename fields directory

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import Connection from './connection/Connection';
 import Entity from './Entity';
-import Column from './fields/Column';
-import Join from './fields/Join';
-import Many from './fields/Many';
-import One from './fields/One';
-import Primary from './fields/Primary';
-import Ref from './fields/Ref';
-import Table from './fields/Table';
+import Column from './instruction/Column';
+import Join from './instruction/Join';
+import Many from './instruction/Many';
+import One from './instruction/One';
+import Primary from './instruction/Primary';
+import Ref from './instruction/Ref';
+import Table from './instruction/Table';
 import MySQLConnection from './mysql/Connection';
 import Query from './query/Query';
 import SQLiteConnection from './sqlite/Connection';
-
-export * from './mysql';
 
 export default Entity;
 export {
