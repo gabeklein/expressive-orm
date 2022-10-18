8fd5735bb323f3802141912d7c46393ea50ce734
Gabe Klein <gabe@termtm.com>
2022-10-18 00:14:27 -0400

    add to insert tests

--- tests/insert.test.ts
+++ tests/insert.test.ts
@@ -1,39 +1,62 @@
-import { Entity, Many, One } from "../src";
+import { Entity, Int, Query, VarChar } from '../src';
+import { TestConnection } from './database';
 
-class Author extends Entity {
-  name = String();
-  books = Many(Book);
-  publisher = One(Publisher);
+class User extends Entity  {
+  name = VarChar();
+  email = VarChar();
+  age = Int();
 }
 
-class Publisher extends Entity {
-  name = String();
+function random(min: number, max: number) {
+  const u = Math.max(min, max);
+  const l = Math.min(min, max);
+  const diff = u - l;
+  const r = Math.floor(Math.random() * (diff + 1)); //'+1' because Math.random() returns 0..0.99, it does not include 'diff' value, so we do +1, so 'diff + 1' won't be included, but just 'diff' value will be.
+  
+  return l + r; //add the random number that was selected within distance between low and up to the lower limit.  
 }
 
-class Book extends Entity {
-  title = String();
-  author = One(Author);
-}
+TestConnection.create([ User ]);
 
-it.skip("will insert procedurally generated rows", () => {
-  const authors: any[] = [];
-  const books: any[] = [];
+it.skip("will insert procedurally generated rows", async () => {
+  const names = [
+    "Gabe",
+    "Justin",
+    "Joe",
+    "Steven",
+    "Josiah"
+  ];
 
-  for(const authorName of ["Gabe", "BJ", "JC", "Danny"]){
-    const author = new Author();
-
-    author.name = authorName;
-    authors.push(author);
-
-    for(const name of ["Foo", "Bar", "Baz"]){
-      const book = new Book();
-
-      book.title = name;
-      book.author = author;
-      
-      books.push(book);
+  await User.insert(names, (name) => {
+    return {
+      name,
+      age: random(20, 35),
+      email: `${name.toLowerCase()}@fennec-engineering.com`
     }
-  }
+  });
+
+  const results = await Query.get(where => where(User))
+
+  void results;
+
+  // const authors: any[] = [];
+  // const books: any[] = [];
+
+  // for(const authorName of ["Gabe", "BJ", "JC", "Danny"]){
+  //   const author = new Author();
+
+  //   author.name = authorName;
+  //   authors.push(author);
+
+  //   for(const name of ["Foo", "Bar", "Baz"]){
+  //     const book = new Book();
+
+  //     book.title = name;
+  //     book.author = author;
+      
+  //     books.push(book);
+  //   }
+  // }
 
   // connection.insert([ ...authors, ...books ])
 })
