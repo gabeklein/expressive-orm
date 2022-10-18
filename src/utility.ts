5924dcc3325c47379f15db90b2a9c1d1a253a654
Gabe Klein <gabe@termtm.com>
2022-10-18 12:53:32 -0400

    remove commented out code

--- src/utility.ts
+++ src/utility.ts
@@ -49,18 +49,3 @@ function asPromise<T = void>(action: asPromise.Action<T>){
 }
 
 export { asPromise };
-
-// export function promisify(fn){
-//   return (...args) => {
-//     return new Promise((resolve, reject) => {
-//       function customCallback(err, ...results) {
-//         if (err) {
-//           return reject(err)
-//         }
-//         return resolve(results.length === 1 ? results[0] : results) 
-//        }
-//        args.push(customCallback)
-//        fn.call(this, ...args)
-//      })
-//   }
-// }
