534200625103123a82b3f1267f8d936e7827fd44
Gabe Klein <gabe@termtm.com>
2022-10-21 02:17:19 -0400

    implement sql parser

---
+++ src/mysql/parse/Parser.ts
@@ -0,0 +1,233 @@
+import Scanner from "./Scanner";
+
+type BunchOf<T = any> = { [key: string]: T }
+
+declare namespace Parser {
+  interface Table {
+    name: string;
+    columns: BunchOf<Column>;
+  }
+
+  interface Column {
+    name: string;
+    datatype: string;
+    primary?: boolean;
+    argument?: string | number | string[];
+    nullable?: boolean;
+    default?: string | number;
+    unique?: boolean;
+    comment?: string;
+    increment?: boolean;
+  }
+}
+
+class Parser {
+  scan: Scanner;
+
+  database?: string;
+  tables = {} as BunchOf<{}>;
+
+  constructor(code: string){
+    this.scan = new Scanner(code);
+  }
+
+  parse(){
+    this.scan.next((token) => {
+      const { type } = token;
+  
+      switch(type){
+        case "keyword":
+          this.statement(token.value);
+          return;
+
+        case "comment":
+        case "space":
+        case "newline":
+        case "end":
+          return;
+  
+        default:
+          throw new Error(`Unexpected ${type}`)
+      }
+    })
+  }
+
+  statement(command: string){
+    const { scan } = this;
+
+    switch(command){
+      case "INSERT":
+        return;
+  
+      case "CREATE": {
+        const { value } = scan.expect("keyword");
+  
+        if(value === "TABLE")
+          this.table();
+  
+        scan.endStatement();
+        return;
+      }
+  
+      case "ALTER":
+        scan.endStatement();
+      break;
+  
+      case "USE": {
+        const name = scan.expect("escaped");
+  
+        if(!this.database)
+          this.database = name.value;
+
+        scan.endStatement();
+        break;
+      }
+  
+      case "DROP":
+        scan.endStatement();
+      break;
+    }
+  }
+
+  table(){
+    const { scan } = this;
+    const name = scan.expect("escaped").value;
+    const table: Parser.Table = {
+      name,
+      columns: {}
+    };
+
+    scan.expect("lparen");
+
+    while(true){
+      if(scan.nextIs("rparen", true))
+        break;
+
+      scan.nextIs("comma", true);
+    
+      if(scan.nextIs("keyword")){
+        this.constraint(table);
+      }
+      else {
+        const column = this.column();
+  
+        table.columns[column.name] = column;
+      }
+    }
+    
+    scan.expect("semi");
+
+    this.tables[name] = table;
+  
+    return {};
+  }
+
+  constraint(table: Parser.Table){
+    const { scan } = this;
+
+    if(scan.expect("keyword").value !== "CONSTRAINT")
+      throw new Error(`Unexpected thing.`)
+
+    scan.expect("escaped");
+    scan.expect("keyword");
+    scan.expect("keyword");
+    scan.expect("lparen");
+
+    const name = scan.expect("escaped").value;
+
+    scan.expect("rparen");
+
+    table.columns[name].primary = true;
+  }
+
+  column(){
+    const { scan } = this;
+
+    const name = scan.expect(["escaped", "word"]).value;
+    const datatype = scan.expect(["keyword", "word"]).value;
+
+    const info = { name, datatype } as Parser.Column;
+
+    if(scan.nextIs("lparen"))
+      info.argument = this.typeAttributes();
+
+    while(true){
+      if(scan.nextIs("comma", true) || scan.nextIs("rparen"))
+        break;
+
+      const next = scan.expect(["word", "keyword"]);
+
+      switch(next.value){
+        case "NOT NULL":
+          info.nullable = false;
+        break;
+
+        case "UNIQUE":
+          info.unique = true;
+        break;
+
+        case "PRIMARY":
+          info.primary = true;
+        break;
+
+        case "AUTO_INCREMENT":
+          info.increment = true;
+        break;
+
+        case "COMMENT":
+          info.comment = scan.expect(["string", "quote"]).value;
+        break;
+
+        case "DEFAULT": {
+          const next = scan.next();
+
+          switch(next.type){
+            case "string":
+            case "number":
+            case "word":
+              info.default = next.value;
+            break;
+
+            case "lparen":
+              throw new Error("DEFAULT expression not yet supported.");
+
+            default:
+              throw new Error(`Unexpected ${next.type}`);
+          }
+
+          break;
+        }
+
+        default:
+          throw new Error(`Unexpected keyword ${next.value}`)
+      }
+    }
+    
+    return info;
+  }
+
+  typeAttributes(){
+    const { scan } = this;
+    const list = [] as any[];
+
+    scan.expect("lparen");
+
+    scan: while(true){
+      const next = scan.next();
+
+      switch(next.type){
+        case "rparen":
+          break scan;
+        case "comma":
+          continue scan;
+        case "number":
+        case "string":
+          list.push(next.value);
+      }
+    }
+
+    return list.length == 1 ? list[0] : list;
+  }
+}
+
+export default Parser;
---
+++ src/mysql/parse/Scanner.ts
@@ -0,0 +1,128 @@
+import moo from "moo";
+import { matchers, Symbols } from "./grammar";
+
+declare namespace Scanner {
+  type Type = Symbols;
+
+  type Token<T extends Type = Type> = moo.Token & { type: T };
+
+  type Next = Token | { type: "end" };
+
+  type Matcher<R = any> = {
+    [P in Type]?: (token: Token) => R;
+  }
+}
+
+class Scanner {
+  lexer: moo.Lexer;
+  buffer?: Scanner.Next;
+
+  constructor(public code: string){
+    this.lexer = moo.compile(matchers).reset(code);
+  }
+
+  look(ignore?: Scanner.Type[] | boolean){
+    if(ignore)
+      this.skip(ignore);
+
+    if(this.buffer)
+      return this.buffer;
+
+    return this.buffer = this.next();
+  }
+
+  next(): Scanner.Next;
+  next<T>(check: (next: Scanner.Next) => T | undefined): T;
+  next<T>(checkMultiple: (next: Scanner.Next) => (next: Scanner.Next) => T): T;
+  next(check?: Function){
+    const next = this.buffer || this.lexer.next() || { type: "EOF" };
+
+    this.buffer = undefined;
+  
+    if(!check)
+      return next;
+
+    let result = check(next);
+
+    if(typeof result == "function"){
+      check = result as Function;
+      result = undefined;
+    }
+
+    while(!result){
+      const next = this.lexer.next();
+      result = check(next || { type: "end" });
+
+      if(!result && !next)
+        return;
+    }
+
+    return result;
+  }
+
+  nextIs<T extends Scanner.Type>(
+    type: T, flush?: boolean
+  ){
+    this.skip();
+
+    const next = this.look();
+
+    if(next.type == type){
+      if(flush)
+        this.next();
+
+      return next as Scanner.Token<T>;
+    }
+  }
+
+  expect<T extends Scanner.Type>(expect: T | T[], ignoreWhitespace: boolean): Scanner.Token<T>;
+  expect<T extends Scanner.Type>(expect: T | T[], ignore?: Scanner.Type[]): Scanner.Token<T>;
+  expect<R>(match: Scanner.Matcher<R>): R;
+  expect(
+    filter?: Scanner.Type | Scanner.Type[] | Scanner.Matcher,
+    ignore?: Scanner.Type[] | boolean){
+
+    if(ignore !== false)
+      this.skip(ignore);
+
+    return this.next((token: Scanner.Next) => {
+      const type = token.type as Scanner.Type;
+
+      if(Array.isArray(filter)){
+        if(filter.includes(type))
+          return token;
+      }
+      else if(typeof filter == "object"){
+        const handle = filter[type]
+
+        if(handle)
+          handle(token as any);
+      }
+      else if(type == filter)
+        return token;
+
+      const where = "line" in token ? ` at line ${token.line}` : "";
+  
+      throw new Error(`Unexpected ` + type + where);
+    })
+  }
+
+  skip(types?: Scanner.Type[] | true){
+    let buffer;
+
+    if(typeof types !== "object")
+      types = ["comment", "linecomment", "newline", "space"];
+
+    do { buffer = this.next() }
+    while(types.includes(buffer.type));
+
+    this.buffer = buffer;
+  }
+
+  endStatement(){
+    while(this.next().type !== "semi")
+      continue;
+  }
+}
+
+export default Scanner;
