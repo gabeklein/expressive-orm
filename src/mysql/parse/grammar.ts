75359385308e27388d68cd27924e9bfa727321d6
Gabe Klein <gabe@termtm.com>
2022-10-21 02:25:50 -0400

    syntax

--- src/mysql/parse/grammar.ts
+++ src/mysql/parse/grammar.ts
@@ -13,13 +13,17 @@ export type Symbols =
   | "newline"
   | "end";
 
+function trim(raw: string){
+  return raw.slice(1, -1);
+}
+
 export const matchers: moo.Rules = {
   space: /[ \t]+/,
   comment: { match: /--[^\r\n]*\r?\n|\/\*[\W\w]+?\*\//, lineBreaks: true },
   number:  { match: /0|[1-9][0-9]*/, value: x => Number(x) as any },
-  string: { match: /"(?:\\"|[^\r\n])*?"/, value: x => x.slice(1, -1) },
-  quote: { match: /'(?:\\'|[^\r\n])*?'/, value: x => x.slice(1, -1) },
-  escaped: { match: /`(?:\\`|[^\r\n])*?`/, value: x => x.slice(1, -1) },
+  string: { match: /"(?:\\"|[^\r\n])*?"/, value: trim },
+  quote: { match: /'(?:\\'|[^\r\n])*?'/, value: trim },
+  escaped: { match: /`(?:\\`|[^\r\n])*?`/, value: trim },
   semi: ';',
   lparen: '(',
   rparen: ')',
