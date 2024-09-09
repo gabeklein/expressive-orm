2c50e2fb7a5dd96da1651335c547a20f33c8a946
Gabe Klein <gabe@termtm.com>
2024-09-09 00:26:06 -0400

    add nested test to where suite

--- packages/sql/tests/where.test.ts
+++ packages/sql/tests/where.test.ts
@@ -10,61 +10,79 @@ class Bar extends Type {
   color = Str();
 }
 
-it("will emit where clauses", () => {
-  class Test extends Type {
-    a = Num();
-    b = Num();
-    c = Num();
-    d = Num();
-  }
-
-  const query = new Query(where => {
-    const test = where(Test);
-
-    where(test.a).is(1);
-    where(test.b).isNot(2);
-    where(test.c).isMore(3);
-    where(test.d).isLess(4);
-  });
-
-  expect(query).toMatchSnapshot();
-})
-
-it("will assert a joined property's value", () => {
-  const query = new Query(where => {
-    const foo = where(Foo);
-    const bar = where(Bar, { color: foo.color });
-
-    where(bar.value).is(42);
-  });
+describe("basic", () => {
+  it("will emit where clauses", () => {
+    class Test extends Type {
+      a = Num();
+      b = Num();
+      c = Num();
+      d = Num();
+    }
   
-  expect(query).toMatchSnapshot();
-})
-
-it.skip("will assert an implictly joined value", () => {
-  class Bar extends Type {
-    foo = One(Foo);
-    greeting = "Hello World";
-  }
-
-  const query = new Query(where => {
-    const bar = where(Bar);
-
-    where(bar.foo.color).is("blue");
-  });
+    const query = new Query(where => {
+      const test = where(Test);
   
-  expect(query).toMatchInlineSnapshot();
+      where(test.a).is(1);
+      where(test.b).isNot(2);
+      where(test.c).isMore(3);
+      where(test.d).isLess(4);
+    });
+  
+    expect(query).toMatchSnapshot();
+  })
+  
+  it("will assert a joined property's value", () => {
+    const query = new Query(where => {
+      const foo = where(Foo);
+      const bar = where(Bar, { color: foo.color });
+  
+      where(bar.value).is(42);
+    });
+    
+    expect(query).toMatchSnapshot();
+  })
+  
+  it.skip("will assert an implictly joined value", () => {
+    class Bar extends Type {
+      foo = One(Foo);
+      greeting = "Hello World";
+    }
+  
+    const query = new Query(where => {
+      const bar = where(Bar);
+  
+      where(bar.foo.color).is("blue");
+    });
+    
+    expect(query).toMatchInlineSnapshot();
+  })
+  
+  it("will match values via object", () => {
+    const query = new Query(where => {
+      const foo = where(Foo);
+  
+      where(foo).has({
+        name: "Gabe",
+        color: "blue"
+      })
+    });
+  
+    expect(query).toMatchSnapshot();
+  })
 })
 
-it("will match values via object", () => {
-  const query = new Query(where => {
-    const foo = where(Foo);
+describe("nested", () => {
+  it("will match nested values", () => {
+    const query = new Query(where => {
+      const foo = where(Foo);
+      const bar = where(Bar, { color: foo.color });
 
-    where(foo).has({
-      name: "Gabe",
-      color: "blue"
-    })
-  });
+      where.any(() => {
+        where(foo.name).is("Gabe")
+        where(bar.value).is(42)
+      })
+    });
 
-  expect(query).toMatchSnapshot();
+    expect(query).toMatchSnapshot();
+  })
 })
