3a60c90814cff8a56c9a99d310c47cec03ae8680
Gabe Klein <gabe@termtm.com>
2024-09-25 16:47:10 -0400

    Delete join instruction

--- packages/sql/src/columns/index.ts
+++ packages/sql/src/columns/index.ts
@@ -1,6 +1,5 @@
 export { Bool } from './Bool';
 export { Enum } from './Enum';
-export { Join } from './Join';
 export { Json } from './Json';
 export { Many } from './Many';
 export { Num } from './Num';
