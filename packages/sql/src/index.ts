fad0f7a5b32ad68f13c5a31442fc1c7e81bbc5d6
Gabe Klein <gabe@termtm.com>
2024-12-21 17:53:33 -0600

    export more exports

--- packages/sql/src/index.ts
+++ packages/sql/src/index.ts
@@ -1,6 +1,6 @@
 export { Connection } from './connection/Connection';
-export { Field } from './Field';
-export { Query } from './Query';
+export { Field, Nullable, Optional } from './Field';
+export { Query, SelectQuery } from './query/Query';
 export { Type } from './Type';
 export { Table } from './Table';
 
