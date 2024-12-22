0863b7b9a361f69965b443046be91b79e735d77e
Gabe Klein <gabe@termtm.com>
2024-12-21 21:56:00 -0600

    reorder exports

--- packages/sql/src/columns/index.ts
+++
@@ -1,5 +0,0 @@
-export { Bool } from './Bool';
-export { Num } from './Num';
-export { One } from './One';
-export { Str } from './Str';
-export { Time } from './Time';
--- packages/sql/src/index.ts
+++ packages/sql/src/index.ts
@@ -1,7 +1,11 @@
+export { Bool } from './columns/Bool';
+export { Num } from './columns/Num';
+export { One } from './columns/One';
+export { Str } from './columns/Str';
+export { Time } from './columns/Time';
+
+export { Type } from './type/Type';
+export { Field, Nullable, Optional } from './type/Field';
 export { Connection } from './connection/Connection';
 export { Query, SelectQuery } from './query/Query';
-export { Field, Nullable, Optional } from './type/Field';
 export { Table } from './type/Table';
-export { Type } from './type/Type';
-
-export * from  './columns';
