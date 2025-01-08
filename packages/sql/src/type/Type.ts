8ac6a5bad7105a914d15772532d3de2f673876f4
Gabe Klein <gabe@termtm.com>
2025-01-08 13:50:59 -0600

    better overloads for Type insert

--- packages/sql/src/type/Type.ts
+++ packages/sql/src/type/Type.ts
@@ -64,9 +64,10 @@ abstract class Type {
     return fields(this);
   }
 
-  static insert<T extends Type>(this: Type.EntityType<T>, entries: Type.Insert<T> | Type.Insert<T>[]): Type.InsertOp;
+  static insert<T extends Type>(this: Type.EntityType<T>, values: Type.Insert<T>): Type.InsertOp;
+  static insert<T extends Type>(this: Type.EntityType<T>, values: Type.Insert<T>[]): Type.InsertOp;
+  static insert<T extends Type, I>(this: Type.EntityType<T>, forEach: Array<I>, map: (value: I, index: number) => Type.Insert<T>): Type.InsertOp;
   static insert<T extends Type>(this: Type.EntityType<T>, number: number, map: (index: number) => Type.Insert<T>): Type.InsertOp;
-  static insert<T extends Type, I>(this: Type.EntityType<T>, inputs: Array<I>, map: (value: I, index: number) => Type.Insert<T>): Type.InsertOp;
   static insert<T extends Type>(
     this: Type.EntityType<T>,
     arg1: Type.Insert<T> | Iterable<unknown> | number,
