c0c4709c58c9f1519bfec2d13ae872c4444f3141
Gabe Klein <gabe@termtm.com>
2025-03-27 17:40:17 -0400

    support Table callback

--- packages/sql/src/type/Table.ts
+++ packages/sql/src/type/Table.ts
@@ -2,22 +2,27 @@ import { assign } from '../utils';
 import { Field } from './Field';
 import { Type } from './Type';
 
-namespace Table {
-  export interface Options {
+declare namespace Table {
+  interface Options {
     table?: string;
     schema?: string;
   }
+
+  type Callback = (self: Type.EntityType) => Options | void;
 }
 
 function Table(name: string, opts?: Table.Options): Type.EntityType;
 function Table(opts: Table.Options): Type.EntityType;
-function Table(arg1: string | Table.Options, arg2?: Table.Options){
+function Table(callback: Table.Callback): Type.EntityType;
+function Table(arg1: string | Table.Options | Table.Callback, arg2?: Table.Options){
   return Field.does((parent) => {
     if(typeof arg1 == "string"){
       const [table, schema] = arg1.split(".").reverse();
 
       arg1 = { schema, ...arg2, table };
     }
+    else if(typeof arg1 == "function")
+      arg1 = arg1(parent) || {};
 
     assign(parent, arg1);
   }) as any;
