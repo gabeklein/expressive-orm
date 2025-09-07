172806a7ab8d9ccb401e3508952a361b043e1e9a
Gabe Klein <gabe@termtm.com>
2025-09-07 13:20:25 -0400

    chore(orm): add more tests

--- packages/orm/tests/get.test.ts
+++ packages/orm/tests/get.test.ts
@@ -1,22 +1,66 @@
-import { Table, str } from "./Table";
+import { Table, str, asc, greaterThan } from "./Table";
 
-class User extends Table {
-  static table = 'users';
+describe("one", () => {
+  class User extends Table {
+    static table = "users";
 
-  name = str();
-}
+    name = str();
+  }
 
-it("will get a record by id", async () => {
-  await Table.connection.exec(`
-    CREATE TABLE IF NOT EXISTS users (
-      id SERIAL PRIMARY KEY,
-      name TEXT
-    );
+  beforeAll(() => {
+    Table.connection.reset();
+    Table.connection.exec(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name TEXT
+      );
 
-    INSERT INTO users (id, name) VALUES (1, 'Gabe')
-  `)
+      INSERT INTO users (id, name)
+      VALUES (1, 'Gabe'), (2, 'Alice'), (3, 'Bob');
+    `);
+  });
 
-  const user = await User.one();
-  expect(user.id).toBe(1);
-  expect(user.name).toBe('Gabe');
+  it("will get record by id", async () => {
+    const user = await User.one(2);
+
+    expect(user.id).toBe(2);
+  });
+
+  it("will get latest record", async () => {
+    const user = await User.one();
+
+    expect(user.id).toBe(3);
+  });
+
+  it("will get ascending result", async () => {
+    const user = await User.one({ id: asc() });
+
+    expect(user.id).toBe(1);
+  });
+
+  it("will both match and sort", async () => {
+    const user = await User.one({
+      id: [asc(), greaterThan(1)],
+    });
+
+    expect(user.id).toBe(2);
+  });
+
+  it("will search for match", async () => {
+    const user = await User.one({ name: "Gabe" });
+
+    expect(user.id).toBe(1);
+  });
+
+  it("will throw on no match", async () => {
+    const query = User.one({ name: "Foo" });
+
+    await expect(query).rejects.toThrow();
+  });
+
+  it("will return undefined if optional", async () => {
+    const user = await User.one({ name: "Foo" }, false);
+
+    expect(user).toBeUndefined();
+  });
 });
